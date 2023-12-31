10d15c96e9962c997a0486f5f41fbdf6c418c7dc
bulentoral <96498139+bulent017@users.noreply.github.com>
2024-01-01 01:47:30 +0300

    Post modeli düzenlendi, Diğer gerksiz model classları silindi.

--- app/src/main/java/com/bulentoral/pawpal/model/Post.java
+++ app/src/main/java/com/bulentoral/pawpal/model/Post.java
@@ -3,76 +3,101 @@ package com.bulentoral.pawpal.model;
 import java.util.Date;
 
 public class Post {
-    private String postId;
-    private String userId; // İlanı veren kullanıcı
-    private String animalId; // İlgili hayvanın ID'si
-    private PostType postType; // Sahiplendirme veya Kayıp
-    private String title;
+    private String postID;
+
+    private String userID;
+    private String name;
+    private String type;
+    private String genus;
+    private int age;
+    private String gender;
     private String description;
+    private String healthStatus;
+    private String photos;
     private Date datePosted;
-    private boolean status;   // ilan durumu aktif ,güncel
-    private enum PostType {
-        ADOPTION, // Sahiplendirme için
-        LOST // Kayıp hayvan için
-    }
 
-    public Post(String postId, String userId, String animalId, PostType postType, String title, String description, Date datePosted, boolean status) {
-        this.postId = postId;
-        this.userId = userId;
-        this.animalId = animalId;
-        this.postType = postType;
-        this.title = title;
+    // Adoption attributes
+    private boolean adoptionStatus;
+    private Date dateAvailable;
+
+    // Lost aniaml attributes
+    private Date dateLost;
+    private String address;
+    private double award = 0.0; // Opsiyonel
+
+    public Post(String postID,String userID,String name , String type, String genus, int age, String gender, String description, String healthStatus, String photos, boolean adoptionStatus, Date dateAvailable, Date datePosted) {
+        this.userID = userID;
+        this.postID =postID;
+        this.name = name;
+        this.type = type;
+        this.genus = genus;
+        this.age = age;
+        this.gender = gender;
         this.description = description;
+        this.healthStatus = healthStatus;
+        this.photos = photos;
         this.datePosted = datePosted;
-        this.status = status;
+        // Adoption-specific attributes
+        this.adoptionStatus = adoptionStatus;
+        this.dateAvailable = dateAvailable;
+
+        // Initialize lost animal attributes as null or default
+        this.dateLost = null;
+        this.address = null;
+        this.award = 0.0;
     }
 
-    public String getPostId() {
-        return postId;
-    }
-
-    public void setPostId(String postId) {
-        this.postId = postId;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getAnimalId() {
-        return animalId;
-    }
-
-    public void setAnimalId(String animalId) {
-        this.animalId = animalId;
-    }
-
-    public PostType getPostType() {
-        return postType;
-    }
-
-    public void setPostType(PostType postType) {
-        this.postType = postType;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
+    public Post(String postID,String userID, String name, String type, String genus, int age, String gender, String description, String healthStatus, String photos, Date dateLost, String address, double award,Date datePosted) {
+        this.userID = userID;
+        this.postID =postID;
+        this.name = name;
+        this.type = type;
+        this.genus = genus;
+        this.age = age;
+        this.gender = gender;
         this.description = description;
+        this.healthStatus = healthStatus;
+        this.photos = photos;
+        this.datePosted = datePosted;
+        // Initialize adoption attributes as null or default
+        this.adoptionStatus = false;
+        this.dateAvailable = null;
+
+        // Lost animal-specific attributes
+        this.dateLost = dateLost;
+        this.address = address;
+        this.award = award;
+    }
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getPostID() {
+        return postID;
+    }
+
+    public void setPostID(String postID) {
+        this.postID = postID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
     }
 
     public Date getDatePosted() {
@@ -83,11 +108,95 @@ public class Post {
         this.datePosted = datePosted;
     }
 
-    public boolean isStatus() {
-        return status;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public void setStatus(boolean status) {
-        this.status = status;
+    public String getGenus() {
+        return genus;
+    }
+
+    public void setGenus(String genus) {
+        this.genus = genus;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHealthStatus() {
+        return healthStatus;
+    }
+
+    public void setHealthStatus(String healthStatus) {
+        this.healthStatus = healthStatus;
+    }
+
+    public String getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(String photos) {
+        this.photos = photos;
+    }
+
+    public boolean isAdoptionStatus() {
+        return adoptionStatus;
+    }
+
+    public void setAdoptionStatus(boolean adoptionStatus) {
+        this.adoptionStatus = adoptionStatus;
+    }
+
+    public Date getDateAvailable() {
+        return dateAvailable;
+    }
+
+    public void setDateAvailable(Date dateAvailable) {
+        this.dateAvailable = dateAvailable;
+    }
+
+    public Date getDateLost() {
+        return dateLost;
+    }
+
+    public void setDateLost(Date dateLost) {
+        this.dateLost = dateLost;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getAward() {
+        return award;
+    }
+
+    public void setAward(double award) {
+        this.award = award;
     }
 }
--- app/src/main/java/com/bulentoral/pawpal/ui/auth/User.java
+++
@@ -1,9 +0,0 @@
-package com.bulentoral.pawpal.ui.auth;
-
-public class User {
-    private String name;
-    private String surName;
-    private String eMail;
-    private String homeAdress;
-
-}
